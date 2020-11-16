49d1a02bc5d2d834e7d406ed0cdcb5871b4d01f8
Nilou Mostofi <nilou@km-direct.com>
2020-11-15 19:04:25 -0800

    transfering files from old folder

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-github" /> FSC Web Design
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
